55304b106d4c1a913d6e05e7105b2a5c9a3fdc68
agefly <wheatdew@163.com>
2020-08-06 15:28:59 +0800

    添加拼图类

---
+++ Assets/GenIndex/PuzzleScript/Puzzle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Puzzle : MonoBehaviour
+{
+    public string codeString;
+
+    public float width;
+
+    private void Start()
+    {
+        width = transform.localScale.z;
+    }
+
+    public List<Transform> puzzleParts=new List<Transform>();
+    public List<List<Puzzle>> puzzleChild = new List<List<Puzzle>>();
+
+    public void Insert(int positionNumber,Puzzle part)
+    {
+        UpdatePositionOnAddition(positionNumber, part.width);
+        puzzleChild[positionNumber].Add(part);
+        part.transform.position = puzzleParts[positionNumber * 2 - 1].position + new Vector3(0.6f, 0, 0);
+        
+    }
+
+    public void UpdatePositionOnAddition(int positionNumber,float width)
+    {
+        int convertNumber = positionNumber * 2-1;
+        for(int i = 0; i < convertNumber; i++)
+            puzzleParts[i].transform.position += new Vector3(0, 0, width);
+        puzzleParts[convertNumber].localScale += new Vector3(0,0,width*0.1f);
+    }
+
+}
